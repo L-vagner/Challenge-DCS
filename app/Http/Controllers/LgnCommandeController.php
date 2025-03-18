d8ec09e250d4f6eb1c1e7d2e9762c68141268b55
Ludovic Vagner <ludovicvagner@yahoo.fr>
2025-03-18 11:31:46 +0100

    Initial commit

---
+++ app/Http/Controllers/LgnCommandeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\LigneCommande;
+
+class LgnCommandeController extends Controller
+{
+    public function randomTest()
+    {
+
+    }
+}
